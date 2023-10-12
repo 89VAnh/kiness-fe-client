1ba3f78a4801f03f18f999fe1a9b5a3ece9c6c1d
Le Minh <minhld@aiacademy.edu.vn>
2023-10-12 16:35:10 +0700

    add register form

--- src/modules/user/register-member/RegisterMember.tsx
+++ src/modules/user/register-member/RegisterMember.tsx
@@ -2,10 +2,13 @@ import {
   Button,
   Checkbox,
   Col,
+  DatePicker,
   Divider,
   Form,
   Input,
+  Radio,
   Row,
+  Select,
   Typography,
 } from "antd";
 import { useTranslation } from "react-i18next";
@@ -26,74 +29,153 @@ export default function RegisterMember(): JSX.Element {
   const renderChildren = () => {
     return (
       <div className={styles.container}>
-        <div className={styles.wrapper}>
-          <Typography.Title level={3}>Đăng nhập</Typography.Title>
-          <Divider />
-          <Form className={styles.content}>
-            <Row gutter={27}>
-              <Col span={16}>
-                <div className={styles.contentForm}>
-                  <Form.Item
-                    name={"username"}
-                    label={"Tài khoản"}
-                    rules={[...RULES_FORM.required]}
-                  >
-                    <Input placeholder={t("user.username") || ""} />
-                  </Form.Item>
-                  <Form.Item
-                    name={"password"}
-                    label={t("user.password")}
-                    rules={[...RULES_FORM.required]}
-                  >
-                    <Input.Password placeholder={t("user.username") || ""} />
-                  </Form.Item>
-                  <Checkbox>Lưu tài khoản </Checkbox>
-                </div>
-              </Col>
-              <Col span={8}>
-                <div className={styles.btnSubmit}>
-                  <Button type="text" htmlType="submit" size="large">
-                    Đăng nhập
-                  </Button>
-                </div>
-              </Col>
-            </Row>
-            <Divider />
-            <div className={styles.wrapperOption}>
-              <Row
-                gutter={27}
-                style={{ alignItems: "center", marginBottom: 12 }}
-              >
-                <Col span={16}>
-                  <div>
-                    <Typography.Text>Quên Tài khoản/Mật khẩu?</Typography.Text>
-                  </div>
-                </Col>
-                <Col span={8}>
-                  <div>
-                    <Button className={styles.btnAmber} type="text">
-                      Tìm tài khoản/mật khẩu
-                    </Button>
-                  </div>
-                </Col>
-              </Row>
-              <Row gutter={27} style={{ alignItems: "center" }}>
-                <Col span={16}>
-                  <div>
-                    <Typography.Text style={{ whiteSpace: "nowrap" }}>
-                      Nếu bạn chưa có tài khoản, vui lòng đăng ký làm thành viên
-                    </Typography.Text>
-                  </div>
-                </Col>
-                <Col span={8}>
-                  <Link to={REGISTER_URL}>
-                    <Button type="text">Đăng ký tài khoản</Button>
-                  </Link>
-                </Col>
-              </Row>
-            </div>
-          </Form>
-        </div>
+        <Typography.Title level={3}>
+          Đăng ký tham gia thành viên KINESS
+        </Typography.Title>
+        <Divider />
+        <Form className={styles.content}>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Trung tâm Kiness
+              </Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item>
+                <Select placeholder="Chọn tỉnh, thành phố" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item>
+                <Select placeholder="Chọn chi nhánh" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Tên tài khoản
+              </Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item>
+                <Input placeholder="Tên tài khoản" />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Typography.Text type="secondary">
+                (Tối đa 6-8 chữ/số)
+              </Typography.Text>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Mật khẩu
+              </Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item>
+                <Input.Password placeholder="Mật khẩu" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Xác nhận mật khẩu
+              </Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item>
+                <Input.Password placeholder=" Xác nhận mật khẩu" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Tên thành viên
+              </Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item>
+                <Input placeholder="Tên thành viên" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Ngày sinh
+              </Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item>
+                <DatePicker
+                  style={{ width: "100%" }}
+                  format={"dd/mm/yyyy"}
+                  placeholder="dd/mm/yyyy"
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Giới tính
+              </Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item initialValue={1}>
+                <Radio.Group defaultValue={1}>
+                  <Radio value={1}>Nam</Radio>
+                  <Radio value={0}>Nữ</Radio>
+                </Radio.Group>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Địa chỉ
+              </Typography.Text>
+            </Col>
+            <Col span={18}>
+              <Form.Item>
+                <Input placeholder="Địa chỉ" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>
+                Số điện thoại
+              </Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item>
+                <Input placeholder="Số điện thoại" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row gutter={24} className={styles.listItem}>
+            <Col span={6}>
+              <Typography.Text className={styles.label}>Email</Typography.Text>
+            </Col>
+            <Col span={6}>
+              <Form.Item>
+                <Input placeholder="Email" />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row className={styles.footerForm}>
+            <Col span={24}>
+              <Button>{t("all.btn_cancel")}</Button>
+              <Button className="filled" htmlType="submit">{t("all.btn_confirm")}</Button>
+            </Col>
+          </Row>
+        </Form>
       </div>
     );
   };
