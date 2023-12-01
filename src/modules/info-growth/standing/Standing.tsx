f9e92da0699ed5613e659d7cc386b68579677f63
Le Minh <minhld@aiacademy.edu.vn>
2023-12-01 17:23:01 +0700

    added info-growth 50 percent

--- src/modules/info-growth/standing/Standing.tsx
+++ src/modules/info-growth/standing/Standing.tsx
@@ -1,8 +1,10 @@
 import { Typography } from "antd";
 import { motion } from "framer-motion";
 
+import postureImg1 from "@/assets/img/info-growth/img_posture01.png";
 import Breadcrumb from "@/modules/shared/breadcrumb/Breadcrumb";
 import Title from "@/modules/shared/title/Title";
+import DivTransition from "@/modules/shared/transition/DivTransition";
 
 import styles from "./scss/standing.module.scss";
 
@@ -44,6 +46,59 @@ export default function Height(): JSX.Element {
               cơ), liệu pháp giải phóng cân cơ và liệu pháp thư giãn cơ quan.
             </Typography.Paragraph>
           </motion.div>
+        </div>
+      </section>
+      <section className={`${styles.contentWrap} ${styles.postureArea}`}>
+        <div className="inner">
+          <Typography.Title level={5} className={styles.postureTitle}>
+            Cổ thẳng (cổ rùa)/Vai tròn
+          </Typography.Title>
+
+          <DivTransition className={styles.img}>
+            <img src={postureImg1} alt="posture-1" />
+          </DivTransition>
+
+          <DivTransition className={styles.box}>
+            <span>
+              Trong tình huống các cơ xung quanh cột sống bị suy yếu do ít vận
+              động, trẻ ít vận động do học tập hoặc sử dụng thiết bị di động
+              thường phát triển cổ thẳng (cổ rùa) và tư thế vai khom.
+            </span>
+            <br />
+            <br />
+            <span>
+              Khi đầu cúi về phía trước so với đường cột sống, các cơ ở cổ và
+              vai dễ bị cứng và mỏi do phải vật lộn để chịu đựng sức nặng của
+              đầu, trong các tình huống các cơ xung quanh cột sống bị suy yếu do
+              thiếu thể lực. khối lượng lớn, thời gian học tập hoặc sử dụng
+              thiết bị di động tăng lên... Ở trẻ em, cùng với cổ thẳng (cổ rùa),
+              vai cong vào trong, làm tăng căng thẳng về thể chất và cản trở sự
+              phát triển chiều cao. Nếu tình trạng nặng hơn có thể tác động tiêu
+              cực đến việc học và cuộc sống hàng ngày, chẳng hạn như đau đầu và
+              giảm khả năng tập trung trong não do lưu lượng máu lên não giảm.
+            </span>
+            <br />
+            <br />
+            <span>
+              Trong trường hợp vai cong, khoảng trống giữa các xương cánh luôn
+              bị kéo lại, điều này không chỉ làm giảm khả năng vận động của chóp
+              xoay mà còn khiến vai bị đông cứng khi còn trẻ và bị tê ở cánh
+              tay. Sự mất cân bằng ở cổ và vai khiến cột sống ngực bị cong, làm
+              rối loạn tín hiệu và sự tuần hoàn của dây thần kinh trung ương và
+              các mạch máu lớn xung quanh cột sống, cản trở sự phát triển và
+              trong trường hợp nghiêm trọng, chất lượng cuộc sống và khả năng
+              học tập bị giảm sút do đến nỗi đau.
+            </span>
+            <br />
+            <br />
+            <span>
+              Kines là một thiết bị isokinetic không chỉ tăng cường chức năng cơ
+              cột sống một cách nhanh chóng, an toàn và thư giãn các cơ cứng để
+              tăng lưu lượng máu não mà còn tạo điều kiện thuận lợi cho dòng
+              chảy của hệ thần kinh trung ương cơ bản thông qua CST, dạy và duy
+              trì tư thế đúng trong cuộc sống hàng ngày.
+            </span>
+          </DivTransition>
         </div>
       </section>
     </>
